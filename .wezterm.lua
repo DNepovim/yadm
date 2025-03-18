93ec26b8b70471ea92681beff0b59729e92c6fd3
Dominik Bláha <jsem@dominikblaha.cz>
2025-03-18 23:10:31 +0100

    Wez: panes

--- .wezterm.lua
+++ .wezterm.lua
@@ -47,25 +47,10 @@ bar.apply_to_config(config, {
   }
 })
 
--- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
 local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
   return pane:get_user_vars().IS_NVIM == 'true'
 end
 
--- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
--- you have to use this instead, but note that this will not work
--- in all cases (e.g. over an SSH connection). Also note that
--- `pane:get_foreground_process_name()` can have high and highly variable
--- latency, so the other implementation of `is_vim()` will be more
--- performant as well.
-local function is_vim(pane)
-  -- This gsub is equivalent to POSIX basename(3)
-  -- Given "/foo/bar" returns "bar"
-  -- Given "c:\\foo\\bar" returns "bar"
-  local process_name = string.gsub(pane:get_foreground_process_name(), '(.*[/\\])(.*)', '%2')
-  return process_name == 'nvim' or process_name == 'vim'
-end
 
 local direction_keys = {
   h = 'Left',
@@ -100,12 +85,10 @@ config.show_new_tab_button_in_tab_bar = false
 config.font = wezterm.font 'RecMonoCasual Nerd Font Mono'
 
 config.keys = {
-    -- move between split panes
     split_nav('move', 'h'),
     split_nav('move', 'j'),
     split_nav('move', 'k'),
     split_nav('move', 'l'),
-    -- resize panes
     split_nav('resize', 'h'),
     split_nav('resize', 'j'),
     split_nav('resize', 'k'),
@@ -132,12 +115,12 @@ config.keys = {
   },
   { 
     key = 'h', 
-    mods = 'ALT', 
+    mods = 'CMD', 
     action = wezterm.action.ActivateTabRelative(-1) 
   },
   { 
     key = 'l', 
-    mods = 'ALT', 
+    mods = 'CMD', 
     action = wezterm.action.ActivateTabRelative(1) 
   },
   { 
@@ -145,26 +128,6 @@ config.keys = {
     mods = 'CMD', 
     action = wezterm.action.ActivateCommandPalette 
   },
---  {
---    key = 'h',
---    mods = 'CTRL',
---    action = wezterm.action.ActivatePaneDirection 'Left',
---  },
---  {
---    key = 'l',
---    mods = 'CTRL',
---    action = wezterm.action.ActivatePaneDirection 'Right',
---  },
---  {
---    key = 'k',
---    mods = 'CTRL',
---    action = wezterm.action.ActivatePaneDirection 'Up',
---  },
---  {
---    key = 'j',
---    mods = 'CTRL',
---    action = wezterm.action.ActivatePaneDirection 'Down',
---  },
   { 
     key = 'h', 
     mods = 'SHIFT|ALT', 
