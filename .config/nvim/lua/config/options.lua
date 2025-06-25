6ce74d6f8c529f273cc6f8bed2eb8e1c2ec0f21c
Dominik Bláha <dominik.blaha@applifting.cz>
2025-06-25 16:25:38 +0200

    nvim: fix animation turn off

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-vim.g.snacks_akimate = false
+vim.g.snacks_animate = false
 
 -- take "-" as a part of word
 vim.opt.iskeyword:append("-")
